19677a8c5a5fb07dffb210ab8a2cd6f3f5537507
mateusprodev <mateusferreiraprodev@gmail.com>
2025-06-19 13:30:46 -0300

    Ajustes Mercado Pago e logs detalhados

--- server/server.js
+++ server/server.js
@@ -71,6 +71,26 @@ app.use(cors({
   credentials: true,
 }));
 
+// --- CORS DINÂMICO ---
+const allowedOrigins = [
+  'https://turflow.vercel.app',
+  'http://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 // --- CONSTANTES PARA URLs DE RETORNO ---
