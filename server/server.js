c80e0e30a9faff35d1a034011eea6d4d4cb6f511
mateusprodev <mateusferreiraprodev@gmail.com>
2025-06-19 11:01:43 -0300

    WIP: salvando alterações antes do filter-branch

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ try {
   } else if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
     console.log("Firebase: Tentando inicializar com GOOGLE_APPLICATION_CREDENTIALS (path)...");
     admin.initializeApp({
-      credential: admin.credential.cert(require(process.env.GOOGLE_APPLICATION_CREDENTIALS)),
+"C:\Users\noteb\Downloads\git-filter-repo-2.47.0\git-filter-repo-2.47.0"      credential: admin.credential.cert(require(process.env.GOOGLE_APPLICATION_CREDENTIALS)),
     });
     console.log('Firebase: Admin SDK inicializado com GOOGLE_APPLICATION_CREDENTIALS (path).');
   } else {
